2684d649571c1d91bc7d2d639f2a6d484d5443eb
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-15 15:45:08 +0100

    index 0.3

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="bg-gray-100 text-gray-600 px-6 lg:px-8 py-4 lg:py-5">
   <div class="max-w-screen-xl mx-auto flex justify-between items-center relative">
-    <a href="#" class="block flex-shrink-0">
-      <img class="h-9" src="/images/boxify-logo.svg" alt="logo placeholder">
+    <a href="/" class="block flex-shrink-0">
+      <img class="h-9" src="../assets/images/boxify-logo.svg" alt="logo placeholder">
     </a>
     <div class="flex justify-between space-x-4 md:space-x-10 flex-shrink-1 md:flex-grow-0">
       <nav class="hidden md:flex flex-col justify-center items-center p-8 md:p-0 space-y-4 md:space-y-0 md:flex-row md:space-x-4 lg:space-x-8 font-semibold absolute top-16 md:top-0 left-0 w-full md:w-auto  md:relative bg-gray-200 md:bg-transparent">
-        <a href="#" class="hover:opacity-70">Home</a>
-        <a href="#" class="hover:opacity-70">Services</a>
+        <a href="/" class="hover:opacity-70">Home</a>
+        <a href="/about" class="hover:opacity-70">About</a>
         <a href="#" class="hover:opacity-70">Pricing</a>
         <a href="#" class="hover:opacity-70">Blog</a>
         <a href="#" class="hover:opacity-70">Contact</a>
@@ -26,13 +26,12 @@
     <div class="flex md:justify-end col-span-2 md:col-span-1">
       <div class="max-w-md md:pt-4">
         <p class="text-lg text-gray-600">You've got to get enough sleep. Other travelling salesmen live a life of luxury. For instance.</p>
-        <a href="#" class="font-semibold mt-3 inline-block">Learn more</a>
+        <a href="/" class="font-semibold mt-3 inline-block">Learn more</a>
       </div>
     </div>
   </div>
-
   <div class="max-w-screen-xl mx-auto mb-12">
-    <img src="/images/placeholder-horizontal.svg" alt="image placeholder" class="rounded-lg">
+    <img src="../assets/images/placeholder-horizontal.svg" alt="image placeholder" class="rounded-lg">
   </div>
 </div>
 </template>
