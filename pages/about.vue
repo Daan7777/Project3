bf3d94d9c3836f0400544e7faa05617f7e1faf8d
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-15 15:58:48 +0100

    about 0.3

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1> about pagina </h1>
+<div class="bg-gray-100 text-gray-600 px-6 lg:px-8 py-4 lg:py-5">
+  <div class="max-w-screen-xl mx-auto flex justify-between items-center relative">
+    <a href="#" class="block flex-shrink-0">
+      <img class="h-9" src="../assets/images/boxify-logo.svg" alt="logo placeholder">
+    </a>
+    <div class="flex justify-between space-x-4 md:space-x-10 flex-shrink-1 md:flex-grow-0">
+      <nav class="hidden md:flex flex-col justify-center items-center p-8 md:p-0 space-y-4 md:space-y-0 md:flex-row md:space-x-4 lg:space-x-8 font-semibold absolute top-16 md:top-0 left-0 w-full md:w-auto  md:relative bg-gray-200 md:bg-transparent">
+        <a href="/" class="hover:opacity-70">Home</a>
+        <a href="/about" class="hover:opacity-70">About</a>
+        <a href="#" class="hover:opacity-70">Pricing</a>
+        <a href="#" class="hover:opacity-70">Blog</a>
+        <a href="#" class="hover:opacity-70">Contact</a>
+      </nav>
+      <button class="w-9 bg-gray-600 p-2 rounded-md text-gray-200 block md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+        </svg>
+      </button>
+    </div>
+  </div>
+  <div class="max-w-screen-xl mx-auto text-center mt-8 lg:mt-20 xl:mt-32 mb-8 lg:mb-20 xl:mb-32">
+    <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-700 font-medium">High quality themes and websites</h2>
+    <p class="text-xl  text-gray-600 mt-4 max-w-3xl mx-auto">World among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath</p>
+    <div class="flex flex-row space-x-2 justify-center mt-6">
+      <a href="#" class="inline-block bg-gray-600 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white mt-6">Order now</a>
+      <a href="#" class="inline-block bg-gray-500 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white mt-6">View pricing</a>
+    </div>
+    <div class="md:grid md:grid-cols-3 md:gap-12 lg:gap-16 mt-12 lg:mt-20 space-y-12 md:space-y-0">
+      <div class="text-left">
+        <h3 class="font-semibold text-lg mb-2">A collection of textile samples</h3>
+        <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself It showed transformed in his bed into a horrible vermin.</p>
+      </div>
+      <div class="text-left">
+        <h3 class="font-semibold text-lg mb-2">How about if I sleep a little bit</h3>
+        <p>Doing business like this takes much more effort than doing your own business at home, and on top of that there's the curse of travelling.</p>
+      </div>
+      <div class="text-left">
+        <h3 class="font-semibold text-lg mb-2">He felt a slight itch up on his bell</h3>
+        <p>He must have tried it a hundred times, shut his eyes so that he wouldn't have to look at the floundering legs, and only stopped when.</p>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
