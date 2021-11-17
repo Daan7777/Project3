e618781dcf5fde8317e46101cd493eb5147cdc1e
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-17 16:00:21 +0100

    header 0.1

---
+++ components/header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="navbar nuxt-link-active">
+        <NuxtLink to="/" target="_blank" prefetch>Home page</NuxtLink>
+        <NuxtLink to="/about" target="_blank" prefetch>about</NuxtLink>
+    </div>
+</template>
