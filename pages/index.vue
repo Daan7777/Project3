128dd1477dd03c5c9365cf218e5326de783fdd76
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-17 16:00:52 +0100

    index 0.4

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,14 @@
 <template>
-<div class="bg-gray-100 text-gray-600 px-6 lg:px-8 py-4 lg:py-5">
-  <div class="max-w-screen-xl mx-auto flex justify-between items-center relative">
-    <a href="/" class="block flex-shrink-0">
-      <img class="h-9" src="../assets/images/boxify-logo.svg" alt="logo placeholder">
-    </a>
-    <div class="flex justify-between space-x-4 md:space-x-10 flex-shrink-1 md:flex-grow-0">
-      <nav class="hidden md:flex flex-col justify-center items-center p-8 md:p-0 space-y-4 md:space-y-0 md:flex-row md:space-x-4 lg:space-x-8 font-semibold absolute top-16 md:top-0 left-0 w-full md:w-auto  md:relative bg-gray-200 md:bg-transparent">
-        <a href="/" class="hover:opacity-70">Home</a>
-        <a href="/about" class="hover:opacity-70">About</a>
-        <a href="#" class="hover:opacity-70">Pricing</a>
-        <a href="#" class="hover:opacity-70">Blog</a>
-        <a href="#" class="hover:opacity-70">Contact</a>
-      </nav>
-      <button class="w-9 bg-gray-600 p-2 rounded-md text-gray-200 block md:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
-        </svg>
-      </button>
+  <main>
+    <div class="navbar nuxt-link-active">
+      <NuxtLink to="/" target="_blank" prefetch>Home page</NuxtLink>
+      <NuxtLink to="/about" target="_blank" prefetch>about</NuxtLink>
     </div>
-  </div>
-  <div class="max-w-screen-xl mx-auto grid grid-cols-2 gap-6 mb-8 lg:mb-20 mt-12 lg:mt-20">
-    <div class="col-span-2 md:col-span-1">
-      <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-700 font-medium">Amazing Features for your next project or website</h2>
-    </div>
-    <div class="flex md:justify-end col-span-2 md:col-span-1">
-      <div class="max-w-md md:pt-4">
-        <p class="text-lg text-gray-600">You've got to get enough sleep. Other travelling salesmen live a life of luxury. For instance.</p>
-        <a href="/" class="font-semibold mt-3 inline-block">Learn more</a>
+      <div>
+        <sidebar />
       </div>
-    </div>
-  </div>
-  <div class="max-w-screen-xl mx-auto mb-12">
-    <img src="../assets/images/placeholder-horizontal.svg" alt="image placeholder" class="rounded-lg">
-  </div>
-</div>
+      <div class="main">
+        <p>tekst</p>
+      </div>
+  </main>
 </template>
