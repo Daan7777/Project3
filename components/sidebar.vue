84be73d82617b5c6d5c07ff878351a71b2554a1a
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-17 16:00:28 +0100

    sidebar 0.1

---
+++ components/sidebar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="sidebar nuxt-link-active">
+        <NuxtLink to="/" target="_blank" prefetch>Home</NuxtLink>
+        <NuxtLink to="/about" target="_blank" prefetch>About</NuxtLink>
+    </div>
+</template>
