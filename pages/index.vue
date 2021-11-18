77f000376725eb6673caeecdae4a0a08574a2a45
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-18 16:15:27 +0100

    index 0.5

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,23 @@
+<script>
+export default {
+  head: {
+    title: 'Home page',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Home page description'
+      }
+    ]
+  }
+}
+</script>
 <template>
   <main>
     <div class="navbar nuxt-link-active">
       <NuxtLink to="/" target="_blank" prefetch>Home page</NuxtLink>
       <NuxtLink to="/about" target="_blank" prefetch>about</NuxtLink>
+      <NuxtLink to="/pagina1" target="_blank" prefetch>Test pagina</NuxtLink>
     </div>
       <div>
         <sidebar />
@@ -12,3 +27,8 @@
       </div>
   </main>
 </template>
+<script>
+export default {
+  transition: 'home'
+}
+</script>
