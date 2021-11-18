ed498c2932897fd483487013d7337ae65cc87c3a
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-18 16:15:37 +0100

    pagina1 0.1

---
+++ pages/pagina1.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  head: {
+    title: 'Test pagina',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Home page description'
+      }
+    ]
+  }
+}
+</script>
+<template>
+  <main>
+    <div>
+      <sidebar />
+    </div>
+    <div class="main">
+      <p>tekst</p>
+    </div>
+  </main>
+</template>
