ff8cb08637a87ccfccabab8684f9aeb4230e3d6b
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-18 16:15:04 +0100

    sidebar 0.2

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -2,5 +2,6 @@
     <div class="sidebar nuxt-link-active">
         <NuxtLink to="/" target="_blank" prefetch>Home</NuxtLink>
         <NuxtLink to="/about" target="_blank" prefetch>About</NuxtLink>
+        <NuxtLink to="/pagina1" target="_blank" prefetch>Test pagina</NuxtLink>
     </div>
 </template>
