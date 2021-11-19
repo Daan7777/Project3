991a68b9fffd704f8fb9fb15fbcebebd3977c9cd
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-19 13:33:24 +0100

    header hernoemd 0.3

--- components/header.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="navbar nuxt-link-active">
-        <NuxtLink to="/" target="_blank" prefetch>Home page</NuxtLink>
-        <NuxtLink to="/about" target="_blank" prefetch>about</NuxtLink>
-        <NuxtLink to="/pagina1" target="_blank" prefetch>Test pagina</NuxtLink>
-    </div>
-</template>
