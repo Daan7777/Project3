9bbe6da59de3ad6e1ca8cc46c9a0391c1ddb03c4
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-19 13:33:14 +0100

    navbar 0.3

---
+++ components/navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="navbar nuxt-link-active">
+        <NuxtLink to="/" target="_blank" prefetch>Home</NuxtLink>
+        <NuxtLink to="/about" target="_blank" prefetch>About</NuxtLink>
+        <NuxtLink to="/pagina1" target="_blank" prefetch>pagina1</NuxtLink>
+    </div>
+</template>
