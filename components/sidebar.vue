3ecb82606d5263ba89c264c80023abeac735759f
Daan7777 <Detvanmontfoort@gmail.com>
2021-11-29 12:50:53 +0100

    sidebar del

--- components/sidebar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="sidebar nuxt-link-active">
-        <NuxtLink to="/" target="_blank" prefetch>Home</NuxtLink>
-        <NuxtLink to="/about" target="_blank" prefetch>About</NuxtLink>
-        <NuxtLink to="/pagina1" target="_blank" prefetch>Test pagina</NuxtLink>
-    </div>
-</template>
