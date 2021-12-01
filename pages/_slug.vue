c46cce93eeafda258a81b29fc8fc95338b03597e
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-01 12:45:51 +0100

    _slug 1.1

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <h1>{{ this.slug }}</h1>
+    <h1>{{ slug }}</h1>
     <p>Path: {{ $route.path }}</p>
   </div>
 </template>
 <script>
-  export default {
-    async asyncData ({ params }) {
-      const slug = await fetch (
-        params.slug
-      )
-      return { slug }
-    }
+export default {
+  async asyncData ({ params }) {
+    const slug = await fetch(
+      params.slug
+    )
+    return { slug }
   }
+}
 </script>
