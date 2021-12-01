f11895c99866b3db4b51e6dbaf28abf08abea693
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-01 12:45:37 +0100

    bootstrap 0.6

--- components/bootstrap.vue
+++ components/bootstrap.vue
@@ -2,13 +2,22 @@
   <main>
     <div>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      (
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"
+      >
+        )
+      </script>
     </div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <a
           class="navbar-brand"
-          href="/">Project 3
+          href="/"
+        >
+          Project 3
         </a>
         <button
           class="navbar-toggler"
@@ -17,35 +26,45 @@
           data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          aria-label="Toggle navigation"
+        >
+          (
+          <span class="navbar-toggler-icon">
+            )
+          </span>
         </button>
         <div
+          id="navbarSupportedContent"
           class="collapse navbar-collapse"
-          id="navbarSupportedContent">
+        >
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a
                 class="nav-link active"
                 aria-current="page"
-                href="/">Home
+                href="/"
+              >
+                Home
               </a>
             </li>
             <li class="nav-item">
               <a
                 class="nav-link"
-                href="/about">About
+                href="/about"
+              >
+                About
               </a>
             </li>
             <li class="nav-item dropdown">
               <a
+                id="navbarDropdown"
                 class="nav-link dropdown-toggle"
                 href="#"
-                id="navbarDropdown"
                 role="button"
                 data-bs-toggle="dropdown"
-                aria-expanded="false">
-                  Dropdown
+                aria-expanded="false"
+              >
+                Dropdown
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li><a class="dropdown-item" href="#">Action</a></li>
@@ -59,7 +78,8 @@
                 class="nav-link disabled"
                 href="#"
                 tabindex="-1"
-                aria-disabled="true">
+                aria-disabled="true"
+              >
                 Disabled
               </a>
             </li>
@@ -69,10 +89,12 @@
               class="form-control me-2"
               type="search"
               placeholder="Search"
-              aria-label="Search">
+              aria-label="Search"
+            >
             <button
               class="btn btn-outline-success"
-              type="submit">
+              type="submit"
+            >
               Search
             </button>
           </form>
