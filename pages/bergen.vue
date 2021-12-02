da759f465bda49845f266396b1e67de750a92344
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-02 14:31:11 +0100

    bergen 0.3

--- pages/bergen.vue
+++ pages/bergen.vue
@@ -1,31 +1,11 @@
 <template>
-<main>
-    <div>
-        <nav>
-            <ul>
-                <li>
-                    <NuxtLink to="/">
-                        Mountains
-                    </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/rivers">
-                        Rivers
-                    </NuxtLink>
-                </li>
-            </ul>
-        </nav>
-        <img src="~/assets/logo.svg">
-        <Nuxt />
-    </div>
-    <div class="main">
-        <ul v-for="mountain in mountains" :key="mountain.id">
-            <NuxtLink :to="`${mountain.continent.toLowerCase()}/${mountain.slug}`">
-            <li>{{ mountain.title }}</li>
-            </NuxtLink>
-        </ul>
-    </div>
-</main>
+  <div>
+    <ul v-for="mountain in mountains" :key="mountain.id">
+      <NuxtLink :to="`${mountain.continent.toLowerCase()}/${mountain.slug}`">
+        <li>{{ mountain.title }}</li>
+      </NuxtLink>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
