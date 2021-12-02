3088fe2a4733204b8d50853eee89601c3827a1ca
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-02 14:03:38 +0100

    index 1.2

--- pages/index.vue
+++ pages/index.vue
@@ -23,41 +23,5 @@ export default {
     <div class="main">
       <p>tekst</p>
     </div>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <NuxtLink to="/">
-              Mountains
-            </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/rivers">
-              Rivers
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
-      <img src="~/assets/logo.svg">
-      <Nuxt />
-    </div>
-    <div class="main">
-      <ul v-for="mountain in mountains" :key="mountain.id">
-        <NuxtLink :to="`${mountain.continent.toLowerCase()}/${mountain.slug}`">
-          <li>{{ mountain.title }}</li>
-        </NuxtLink>
-      </ul>
-    </div>
   </main>
 </template>
-<script>
-export default {
-  async asyncData() {
-    const mountains = await fetch(
-      'https://api.nuxtjs.dev/mountains'
-    ).then((res) => res.json())
-
-    return { mountains }
-  }
-}
-</script>
