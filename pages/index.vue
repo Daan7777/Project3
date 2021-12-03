5d1be253ea1fc95fbfa0a7c7f3bd99ca35f20571
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-03 14:41:36 +0100

    index 1.6

--- pages/index.vue
+++ pages/index.vue
@@ -19,22 +19,22 @@ export default {
     <div>
       <bootstrap />
     </div>
-      <div class="max-w-screen-xl mx-auto text-center mt-8 lg:mt-20 xl:mt-32 mb-8 lg:mb-20 xl:mb-32">
-        <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-700 font-medium">Buy our awesome product now</h2>
-        <p class="text-xl  text-gray-600 mt-4 max-w-3xl mx-auto">World among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath</p>
-        <a href="#" class="inline-block  bg-gray-600 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white mt-6">Order now</a>
-      </div>
-
-      <div class="max-w-screen-xl mx-auto px-6 lg:px-8 xl:px-4 grid grid-cols-2 sm:grid-cols-3 space-y-5 sm:space-y-3 xl:grid-cols-6 col-gap-6 mt-8 lg:mt-10 mb-12 lg:mb-20">
-        <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/boxify-logo.svg" alt="logo placeholder">
-        <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/edge-logo.svg" alt="logo placeholder">
-        <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/sbalbew-logo.svg" alt="logo placeholder">
-        <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/drops-logo.svg" alt="logo placeholder">
-        <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/pathway-logo.svg" alt="logo placeholder">
-        <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/feedback-logo.svg" alt="logo placeholder">
-      </div>
-
+    <div class="max-w-screen-xl mx-auto text-center mt-8 lg:mt-20 xl:mt-32 mb-8 lg:mb-20 xl:mb-32">
+      <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-700 font-medium">Buy our awesome product now</h2>
+      <p class="text-xl  text-gray-600 mt-4 max-w-3xl mx-auto">World among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath</p>
+      <a href="#" class="inline-block  bg-gray-600 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white mt-6">Order now</a>
     </div>
+
+    <div class="max-w-screen-xl mx-auto px-6 lg:px-8 xl:px-4 grid grid-cols-2 sm:grid-cols-3 space-y-5 sm:space-y-3 xl:grid-cols-6 col-gap-6 mt-8 lg:mt-10 mb-12 lg:mb-20">
+      <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/boxify-logo.svg" alt="logo placeholder">
+      <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/edge-logo.svg" alt="logo placeholder">
+      <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/sbalbew-logo.svg" alt="logo placeholder">
+      <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/drops-logo.svg" alt="logo placeholder">
+      <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/pathway-logo.svg" alt="logo placeholder">
+      <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/feedback-logo.svg" alt="logo placeholder">
+    </div>
+
+  </div>
 
     <div class="bg-gray-50 text-gray-600 px-6 lg:px-8 pt-12 pb-20">
       <div class="max-w-screen-xl mx-auto">
