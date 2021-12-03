c0af8ba0affda70908517ec8c8d0ab3d34f35176
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-03 14:55:35 +0100

    index 1.7

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <script>
 import bootstrap from '../components/bootstrap.vue'
+import footer from '../components/footer.vue'
 export default {
-  components: { bootstrap },
+  components: {
+    bootstrap,
+    footer
+  },
   head: {
     title: 'Home page',
     meta: [
@@ -22,7 +26,7 @@ export default {
     <div class="max-w-screen-xl mx-auto text-center mt-8 lg:mt-20 xl:mt-32 mb-8 lg:mb-20 xl:mb-32">
       <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-700 font-medium">Buy our awesome product now</h2>
       <p class="text-xl  text-gray-600 mt-4 max-w-3xl mx-auto">World among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath</p>
-      <a href="#" class="inline-block  bg-gray-600 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white mt-6">Order now</a>
+      <a href="/" class="inline-block  bg-gray-600 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white mt-6">Order now</a>
     </div>
 
     <div class="max-w-screen-xl mx-auto px-6 lg:px-8 xl:px-4 grid grid-cols-2 sm:grid-cols-3 space-y-5 sm:space-y-3 xl:grid-cols-6 col-gap-6 mt-8 lg:mt-10 mb-12 lg:mb-20">
@@ -108,123 +112,8 @@ export default {
       </div>
     </div>
 
-    <div class="bg-gray-50 text-gray-600 px-6 lg:px-8 pt-12 pb-20">
-      <div class="max-w-screen-xl mx-auto grid grid-cols-2 gap-8 lg:gap-24 md:items-center">
-        <div class="col-span-2 md:col-span-1 order-2 md:order-1">
-          <h2 class="text-2xl sm:text-3xl md:text-4xl text-gray-700 font-medium">Join our newsletter</h2>
-          <p class="text-xl text-gray-600 mt-4 max-w-3xl">When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees</p>
-          <form class="flex flex-row space-x-2 mt-6">
-            <input type="text" placeholder="enter email address" class="bg-white border border-gray-300 px-4 py-2 flex-1 rounded-lg">
-            <button class="inline-block bg-gray-600 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white">Subscribe</button>
-          </form>
-        </div>
-        <div class="col-span-2 md:col-span-1 flex flex-row space-x-2 order-1 md:order-2">
-          <img src="/images/placeholder-horizontal.svg" alt="">
-        </div>
-      </div>
+    <div>
+      <footer />
     </div>
-
-    <footer class="bg-gray-50 text-gray-600 px-6 lg:px-8 py-12">
-      <div class="max-w-screen-xl mx-auto mb-12 lg:mb-16  ">
-        <img class="h-8" src="/images/uptimemate-logo-dark.svg" alt="UptimeMate logo">
-      </div>
-      <div class="max-w-screen-xl mx-auto grid md:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-x-8">
-        <div>
-          <h5 class="text-xl font-semibold text-gray-700">Product</h5>
-          <nav class="mt-4">
-            <ul class="space-y-2">
-              <li>
-                <a href="#" class="font-normal text-base hover:text-gray-400">Landingpages</a>
-              </li>
-              <li>
-                <a href="#" class="font-normal text-base hover:text-gray-400">Features</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div>
-          <h5 class="text-xl font-semibold text-gray-700">Industries</h5>
-          <nav class="mt-4">
-            <ul class="space-y-2">
-              <li>
-                <a href="#" class="font-normal text-base hover:text-gray-400">Employment</a>
-              </li>
-              <li>
-                <a href="#" class="font-normal text-base hover:text-gray-400">Childcare</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div>
-          <h5 class="text-xl font-semibold text-gray-700">About us</h5>
-          <nav class="mt-4">
-            <ul class="space-y-2">
-              <li>
-                <a href="#" class="font-normal text-base hover:text-gray-400">Company</a>
-              </li>
-              <li>
-                <a href="#" class="font-normal text-base hover:text-gray-400">Download brochure</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div>
-          <h5 class="text-xl font-semibold text-gray-700">Legal</h5>
-          <nav class="mt-4">
-            <ul class="space-y-2">
-              <li>
-                <a href="#" class="font-normal text-base hover:text-gray-400">Terms and conditions</a>
-              </li>
-              <li>
-                <a href="#" class="font-normal text-base hover:text-gray-400">Security</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-      <div class="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 mt-16 lg:mt-20 border-t-2 border-gray-200 pt-8 ">
-        <div class="text-sm space-y-4 md:space-y-1 text-center md:text-left">
-          <p>©2021 Company. All rights reserved. | All rights reserved</p>
-          <p>Wisdom is easily acquired when hiding under the bed with a saucepan on your head.</p>
-        </div>
-        <div class="flex items-baseline space-x-2">
-          <span>Language: </span>
-          <nav class="text-sm">
-            <a href="#" class="hover:text-gray-400">EN</a>
-            <a href="#" class="hover:text-gray-400">DE</a>
-            <a href="#" class="hover:text-gray-400">NL</a>
-          </nav>
-        </div>
-      </div>
-      <div class="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 mt-8 lg:mt-12">
-        <nav class="flex flex-wrap justify-center space-x-6">
-          <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Privacy</a>
-          <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Content Terms Notice</a>
-          <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Legal</a>
-          <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Features</a>
-          <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Landing Pages</a>
-        </nav>
-        <nav class="flex items-center space-x-2">
-          <a href="#" class="hover:text-gray-500">
-            <span class="sr-only">Facebook</span>
-            <svg class="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-              <path fill-rule="evenodd" d="M22 12c0-5.523-4.XMR492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
-            </svg>
-          </a>
-          <a href="#" class="hover:text-gray-500">
-            <span class="sr-only">Instagram</span>
-            <svg class="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-              <path fill-rule="evenodd" d="M12.315 2c2.XMR 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.XMR-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.XMR218-2.XMR.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.XMR.XMR-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.XMR.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.XMR.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" />
-            </svg>
-          </a>
-      <a href="#" class="hover:text-gray-500">
-            <span class="sr-only">Twitter</span>
-            <svg class="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.XMR 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-            </svg>
-          </a>
-        </nav>
-      </div>
-    </footer>
   </main>
 </template>
