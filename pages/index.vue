b0b1790989dfc3f4f1d4aae031975e0d1e0df714
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-03 14:36:33 +0100

    index 1.3

--- pages/index.vue
+++ pages/index.vue
@@ -14,14 +14,232 @@ export default {
 </script>
 <template>
   <main>
+          <div class="bg-gray-100 text-gray-600 px-6 lg:px-8 py-4 lg:py-5">
+  <div class="max-w-screen-xl mx-auto flex justify-between items-center relative">
+    <a href="#" class="block flex-shrink-0">
+      <img class="h-9" src="/images/boxify-logo.svg" alt="logo placeholder">
+    </a>
+    <div class="flex justify-between space-x-4 md:space-x-10 flex-shrink-1 md:flex-grow-0">
+      <nav class="hidden md:flex flex-col justify-center items-center p-8 md:p-0 space-y-4 md:space-y-0 md:flex-row md:space-x-4 lg:space-x-8 font-semibold absolute top-16 md:top-0 left-0 w-full md:w-auto  md:relative bg-gray-200 md:bg-transparent">
+        <a href="#" class="hover:opacity-70">Home</a>
+        <a href="#" class="hover:opacity-70">Services</a>
+        <a href="#" class="hover:opacity-70">Pricing</a>
+        <a href="#" class="hover:opacity-70">Blog</a>
+        <a href="#" class="hover:opacity-70">Contact</a>
+      </nav>
+      <button class="w-9 bg-gray-600 p-2 rounded-md text-gray-200 block md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+        </svg>
+      </button>
+    </div>
+  </div>
+  <div class="max-w-screen-xl mx-auto text-center mt-8 lg:mt-20 xl:mt-32 mb-8 lg:mb-20 xl:mb-32">
+    <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-700 font-medium">Buy our awesome product now</h2>
+    <p class="text-xl  text-gray-600 mt-4 max-w-3xl mx-auto">World among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath</p>
+    <a href="#" class="inline-block  bg-gray-600 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white mt-6">Order now</a>
+  </div>
+
+  <div class="max-w-screen-xl mx-auto px-6 lg:px-8 xl:px-4 grid grid-cols-2 sm:grid-cols-3 space-y-5 sm:space-y-3 xl:grid-cols-6 col-gap-6 mt-8 lg:mt-10 mb-12 lg:mb-20">
+    <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/boxify-logo.svg" alt="logo placeholder">
+    <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/edge-logo.svg" alt="logo placeholder">
+    <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/sbalbew-logo.svg" alt="logo placeholder">
+    <img class="h-10 p-1 self-end justify-self-center opacity-90" src="/images/drops-logo.svg" alt="logo placeholder">
+    <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/pathway-logo.svg" alt="logo placeholder">
+    <img class="h-12 p-1 self-end justify-self-center opacity-90" src="/images/feedback-logo.svg" alt="logo placeholder">
+  </div>
+
+</div>
+
+<div class="bg-gray-50 text-gray-600 px-6 lg:px-8 pt-12 pb-20">
+  <div class="max-w-screen-xl mx-auto">
+    <h2 class="text-2xl sm:text-3xl md:text-4xl text-gray-700 font-medium text-center">Amazing Features</h2>
+    <p class="text-xl lg:text-2xl text-gray-600 mt-2 text-center">We are mmitted to a unique, cutting-edge approach to that</p>
+  </div>
+  <div class="md:grid md:grid-cols-2 md:gap-12 max-w-screen-xl mx-auto mt-12 lg:mt-20 space-y-12 md:space-y-0">
+    <div class="text-left flex space-x-6">
+      <div class="bg-gray-200 rounded-full w-16 h-16 flex items-center justify-center ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-8">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+        </svg>
+      </div>
+      <div class="flex-1">
+        <h3 class="font-semibold text-lg mb-2">A collection of textile samples</h3>
+        <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself It showed transformed in his bed into a horrible vermin.</p>
+      </div>
+    </div>
+    <div class="text-left flex space-x-6">
+      <div class="bg-gray-200 rounded-full w-16 h-16 flex items-center justify-center ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-8">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+        </svg>
+      </div>
+      <div class="flex-1">
+        <h3 class="font-semibold text-lg mb-2">How about if I sleep a little bit</h3>
+        <p>Doing business like this takes much more effort than doing your own business at home, and on top of that there's the curse of travelling.</p>
+      </div>
+    </div>
+    <div class="text-left flex space-x-6">
+      <div class="bg-gray-200 rounded-full w-16 h-16 flex items-center justify-center ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-8">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
+        </svg>
+      </div>
+      <div class="flex-1">
+        <h3 class="font-semibold text-lg mb-2">He felt a slight itch up on his bell</h3>
+        <p>He must have tried it a hundred times, shut his eyes so that he wouldn't have to look at the floundering legs, and only stopped when.</p>
+      </div>
+    </div>
+    <div class="text-left flex space-x-6">
+      <div class="bg-gray-200 rounded-full w-16 h-16 flex items-center justify-center ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-8">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
+        </svg>
+      </div>
+      <div class="flex-1">
+        <h3 class="font-semibold text-lg mb-2">But who knows, maybe that would do it</h3>
+        <p>You've got to get enough sleep. Other travelling salesmen live a life of luxury. For instance, whenever I go back to the guest.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="bg-gray-50 text-gray-600 px-6 lg:px-8 py-12">
+  <div class="max-w-screen-xl mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-x-8">
+    <div class="bg-gray-200 px-6 pt-20 lg:pt-32 xl:pt-48 pb-6 rounded">
+      <h3 class="font-semibold text-lg mb-2">Collection of textile</h3>
+      <p>One morning, when Gregor Samsa woke from troubled dreams, he found It showed transformed.</p>
+    </div>
+
+    <div class="bg-gray-200 px-6 pt-20 lg:pt-32 xl:pt-48 pb-6 rounded">
+      <h3 class="font-semibold text-lg mb-2">How about sleep a little bit</h3>
+      <p>Doing business like this takes much more effort than doing your own business at home, on top.</p>
+    </div>
+
+    <div class="bg-gray-200 px-6 pt-20 lg:pt-32 xl:pt-48 pb-6 rounded">
+      <h3 class="font-semibold text-lg mb-2">But who knows, would do it</h3>
+      <p>You've got to get enough sleep. Other travelling salesmen live a life of luxury. For instance.</p>
+    </div>
+  </div>
+</div>
+
+<div class="bg-gray-50 text-gray-600 px-6 lg:px-8 pt-12 pb-20">
+  <div class="max-w-screen-xl mx-auto grid grid-cols-2 gap-8 lg:gap-24 md:items-center">
+    <div class="col-span-2 md:col-span-1 order-2 md:order-1">
+      <h2 class="text-2xl sm:text-3xl md:text-4xl text-gray-700 font-medium">Join our newsletter</h2>
+      <p class="text-xl text-gray-600 mt-4 max-w-3xl">When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees</p>
+      <form class="flex flex-row space-x-2 mt-6">
+        <input type="text" placeholder="enter email address" class="bg-white border border-gray-300 px-4 py-2 flex-1 rounded-lg">
+        <button class="inline-block bg-gray-600 font-semibold rounded-lg py-4 px-5 lg:px-8 text-white">Subscribe</button>
+      </form>
+    </div>
+    <div class="col-span-2 md:col-span-1 flex flex-row space-x-2 order-1 md:order-2">
+      <img src="/images/placeholder-horizontal.svg" alt="">
+    </div>
+  </div>
+</div>
+
+<footer class="bg-gray-50 text-gray-600 px-6 lg:px-8 py-12">
+  <div class="max-w-screen-xl mx-auto mb-12 lg:mb-16  ">
+    <img class="h-8" src="/images/uptimemate-logo-dark.svg" alt="UptimeMate logo">
+  </div>
+  <div class="max-w-screen-xl mx-auto grid md:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-x-8">
     <div>
-      <bootstrap />
+      <h5 class="text-xl font-semibold text-gray-700">Product</h5>
+      <nav class="mt-4">
+        <ul class="space-y-2">
+          <li>
+            <a href="#" class="font-normal text-base hover:text-gray-400">Landingpages</a>
+          </li>
+          <li>
+            <a href="#" class="font-normal text-base hover:text-gray-400">Features</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-    <div class="main">
-      <h1>Header tekst</h1>
+    <div>
+      <h5 class="text-xl font-semibold text-gray-700">Industries</h5>
+      <nav class="mt-4">
+        <ul class="space-y-2">
+          <li>
+            <a href="#" class="font-normal text-base hover:text-gray-400">Employment</a>
+          </li>
+          <li>
+            <a href="#" class="font-normal text-base hover:text-gray-400">Childcare</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-    <div class="main">
-      <p>tekst</p>
+    <div>
+      <h5 class="text-xl font-semibold text-gray-700">About us</h5>
+      <nav class="mt-4">
+        <ul class="space-y-2">
+          <li>
+            <a href="#" class="font-normal text-base hover:text-gray-400">Company</a>
+          </li>
+          <li>
+            <a href="#" class="font-normal text-base hover:text-gray-400">Download brochure</a>
+          </li>
+        </ul>
+      </nav>
     </div>
+    <div>
+      <h5 class="text-xl font-semibold text-gray-700">Legal</h5>
+      <nav class="mt-4">
+        <ul class="space-y-2">
+          <li>
+            <a href="#" class="font-normal text-base hover:text-gray-400">Terms and conditions</a>
+          </li>
+          <li>
+            <a href="#" class="font-normal text-base hover:text-gray-400">Security</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <div class="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 mt-16 lg:mt-20 border-t-2 border-gray-200 pt-8 ">
+    <div class="text-sm space-y-4 md:space-y-1 text-center md:text-left">
+      <p>©2021 Company. All rights reserved. | All rights reserved</p>
+      <p>Wisdom is easily acquired when hiding under the bed with a saucepan on your head.</p>
+    </div>
+    <div class="flex items-baseline space-x-2">
+      <span>Language: </span>
+      <nav class="text-sm">
+        <a href="#" class="hover:text-gray-400">EN</a>
+        <a href="#" class="hover:text-gray-400">DE</a>
+        <a href="#" class="hover:text-gray-400">NL</a>
+      </nav>
+    </div>
+  </div>
+  <div class="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 mt-8 lg:mt-12">
+    <nav class="flex flex-wrap justify-center space-x-6">
+      <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Privacy</a>
+      <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Content Terms Notice</a>
+      <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Legal</a>
+      <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Features</a>
+      <a href="#" class="font-normal text-sm hover:text-gray-400 mb-2">Landing Pages</a>
+    </nav>
+    <nav class="flex items-center space-x-2">
+      <a href="#" class="hover:text-gray-500">
+        <span class="sr-only">Facebook</span>
+        <svg class="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M22 12c0-5.523-4.XMR492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
+        </svg>
+      </a>
+      <a href="#" class="hover:text-gray-500">
+        <span class="sr-only">Instagram</span>
+        <svg class="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M12.315 2c2.XMR 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.XMR-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.XMR218-2.XMR.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.XMR.XMR-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.XMR.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.XMR.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" />
+        </svg>
+      </a>
+   <a href="#" class="hover:text-gray-500">
+        <span class="sr-only">Twitter</span>
+        <svg class="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.XMR 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+        </svg>
+      </a>
+    </nav>
+  </div>
+</footer>
   </main>
 </template>
