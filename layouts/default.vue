cfd18d86d7a914188ed76deaaad120d8bb19ecc7
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-10 13:46:09 +0100

    default 1.0

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Mountains</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/rivers">Rivers</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <img src="~/assets/logo.svg" />
+      <Nuxt />
+    </main>
+  </div>
+</template>
