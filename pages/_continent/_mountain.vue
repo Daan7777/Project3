452823eb5cf227c532a1196fb6794b04b92268d4
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-10 13:57:56 +0100

    mountain 1.3

--- pages/_continent/_mountain.vue
+++ pages/_continent/_mountain.vue
@@ -3,9 +3,7 @@
     <h1>Continent: {{ continent }}</h1>
     <h2>Mountain: {{ mountain }}</h2>
     <p>Path: {{ $route.path }}</p>
-    <NuxtLink to="/">
-      Back to Mountains
-    </NuxtLink>
+    <NuxtLink to="/">Back to Mountains</NuxtLink>
   </div>
 </template>
 <script>
@@ -13,13 +11,15 @@ export default {
   async asyncData ({ params, redirect }) {
     const mountains = await fetch(
       'https://api.nuxtjs.dev/mountains'
-    ).then((res) => {
+    ).then(function (res) {
       return res.json()
     })
+
     const filteredMountain = mountains.find(
-      ({ el }) =>
-        el.continent.toLowerCase() === params.continent &&
-        el.slug === params.mountain
+      function (el) {
+        return el.continent.toLowerCase() === params.continent &&
+          el.slug === params.mountain
+      }
     )
     if (filteredMountain) {
       return {
