a15f0096647c1d1fd1a052d2dd4db9cae597390b
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-10 13:57:46 +0100

    bergen 0.6

--- pages/bergen.vue
+++ pages/bergen.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+      <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/mountain">Mountains</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/rivers">Rivers</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <img src="~/assets/logo.svg" />
+      <Nuxt />
+    </main>
+  </div>
     <ul v-for="mountain in mountains" :key="mountain.id">
       <NuxtLink :to="`${mountain.continent.toLowerCase()}/${mountain.slug}`">
         <li>{{ mountain.title }}</li>
@@ -12,7 +28,9 @@ export default {
   async asyncData () {
     const mountains = await fetch(
       'https://api.nuxtjs.dev/mountains'
-    ).then(({ res }) => res.json())
+    ).then(function (res) {
+      return res.json()
+    })
 
     return { mountains }
   }
