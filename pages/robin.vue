2f954cb463436fdf56236872d27ccabacaa44c34
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-16 15:07:32 +0100

    robin 0.2

--- pages/robin.vue
+++ pages/robin.vue
@@ -1,5 +1,36 @@
+<script>
+import bootstrap from '../components/bootstrap.vue'
+import footerVue from '../components/footer.vue'
+import navbar from '../components/navbar.vue'
+export default {
+  components: {
+    bootstrap,
+    footerVue,
+    navbar
+  },
+  head: {
+    title: 'About pagina',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'about page description'
+      }
+    ]
+  }
+}
+</script>
 <template>
   <main>
-    <p> test </p>
+    <div>
+      <bootstrap />
+      <navbar />
+    </div>
+    <div class="main">
+      <p>tekst</p>
+    </div>
+    <div>
+      <footerVue />
+    </div>
   </main>
 </template>
