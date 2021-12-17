86dc8f17df81718778c8ce7e0a0dafec59a5fe24
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-17 15:20:33 +0100

    daan 0.2

---
+++ pages/daan.vue
@@ -0,0 +1,22 @@
+<script>
+import bootstrap from '../components/bootstrap.vue'
+import footerVue from '../components/footer.vue'
+import navbar from '../components/navbar.vue'
+export default {
+  components: {
+    bootstrap,
+    footerVue,
+    navbar
+  },
+  head: {
+    title: 'About pagina',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'about page description'
+      }
+    ]
+  }
+}
+</script>
