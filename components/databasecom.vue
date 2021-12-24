8238ebf72771be9073d6a8d0f2f9f294425ec186
Daan7777 <Detvanmontfoort@gmail.com>
2021-12-24 12:15:25 +0100

    databsecom 0.2

--- components/databasecom.vue
+++ components/databasecom.vue
@@ -1,4 +1,3 @@
 <script>
-export default {
-}
+{"data";[{"id":1,"attributes":{"name":"Biscotte Restaurant","description":"Welcome to Biscotte restaurant! Restaurant Biscotte offers a cuisine based on fresh, quality products, often local, organic when possible, and always produced by passionate producers.\n","createdAt":"2021-12-22T13:18:42.486Z","updatedAt":"2021-12-22T13:25:57.546Z","publishedAt":"2021-12-22T13:25:57.542Z"}}],"meta";{"pagination";{"page";1,"pageSize";25,"pageCount";1,"total";1}}}
 </script>
